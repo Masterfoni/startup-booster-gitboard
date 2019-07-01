0bfebe835fbf662b79fa591e15e1113e0aca5935
Masterfoni <joaovitor_179@hotmail.com>
2019-07-01 17:47:29 -0300

    Add 404 not found page with a link to the root of the app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
+import NotFound from "./pages/not-found/NotFound";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact={true} component={Dashboard} />
+      <Route path="*" exact={true} component={NotFound} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
